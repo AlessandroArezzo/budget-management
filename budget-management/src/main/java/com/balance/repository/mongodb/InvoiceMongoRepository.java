6ec1b3f5c16798ad1ed4704342e303e5bbbc70a8
alessandroarezzo <alessandro.arezzo@stud.unifi.it>
2020-06-29 09:49:38 +0200

    Remove code smells

--- budget-management/src/main/java/com/balance/repository/mongodb/InvoiceMongoRepository.java
+++ budget-management/src/main/java/com/balance/repository/mongodb/InvoiceMongoRepository.java
@@ -21,7 +21,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
-import com.mongodb.client.model.Sorts;
 
 public class InvoiceMongoRepository implements InvoiceRepository{
 
